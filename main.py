8314b1008a437d62ff92b3bf71946fe28ff26711
TS-2210 <148909343+TS-2210@users.noreply.github.com>
2026-06-16 12:06:29 +0100

    module roadmap hardcoded

--- main.py
+++ main.py
@@ -24,7 +24,27 @@ def get_skills_for_career(career, career_data):
         if key == career:
             return val
 
+def score_module(career, career_data, module_data):
+    skills = get_skills_for_career(career, career_data)
+    scores = {}
+    for module in module_data:
+        module_skills = module.get("tags", [])
+        score = len(set(skills) & set(module_skills))
+        scores.update({module["name"]: score})
+    desc = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+    return desc[:3]
+
+def module_roadmap(module_selected, data):
+    prereqs = get_prerequisites(module_selected, data)
+    y1 = [module for module in prereqs if module[4]=="1"]
+    y2 = [module for module in prereqs if module[4]=="2"]
+    print("Year 1:", y1)
+    print("Year 2:", y2)
+    print("Leads to year 3:", module_selected)
+
 data = load_module_data()
 career_data = load_career_data()
 print(get_prerequisites("Cybersecurity: Principles and Secure Software Systems", data))
 print(get_skills_for_career("data scientist", career_data))
+print(score_module("cybersecurity engineer", career_data, data))
+module_roadmap("Natural Language Processing", data)

25304056e40fa27cd0398a9ea56176d547ff0cf1
TS-2210 <148909343+TS-2210@users.noreply.github.com>
2026-06-16 10:59:07 +0100

    career mapping hardcoded

--- get_prereqs.py
+++ get_prereqs.py
@@ -4,6 +4,10 @@ def load_module_data():
     with open("module_data.json", "r") as f:
         return json.load(f)
 
+def load_career_data():
+    with open("careers.json", "r") as f:
+        return json.load(f)
+    
 def get_prerequisites(course, data):
     G = nx.DiGraph()
     prerequisites = []
@@ -15,5 +19,12 @@ def get_prerequisites(course, data):
         prerequisites = list(nx.ancestors(G, course))
     return prerequisites
 
+def get_skills_for_career(career, career_data):
+    for key, val in career_data.items():
+        if key == career:
+            return val
+
 data = load_module_data()
+career_data = load_career_data()
 print(get_prerequisites("Cybersecurity: Principles and Secure Software Systems", data))
+print(get_skills_for_career("data scientist", career_data))
